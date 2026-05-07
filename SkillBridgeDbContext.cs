ef1dbb9e0f268227caccf9e8d624bca1eb2c6b5b
unknown <ja2ck07@gmail.com>
2026-05-07 08:32:36 +0545

    DB connection maybe

---
+++ SkillBridgeDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SkillBridge.Api.Entities;
+
+public class SkillBridgeDbContext : DbContext
+{
+    public SkillBridgeDbContext(DbContextOptions<SkillBridgeDbContext>options) : base(options)
+    {
+        
+    }
+    
+public DbSet<User> Users { get; set;}
+}
